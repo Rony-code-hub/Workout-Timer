232a4ab740d14d63bb1e5ee3cb2c2c668436f6a5
Mahfuj Ahmed Rony <105776505+Rony-code-hub@users.noreply.github.com>
2024-08-26 23:23:25 +0600

    Add files via upload
    I developed the Workout Timer application and customized it by adding callback function, memo, and useMemo hooks.

---
+++ src/ToggoleSounds.js
@@ -0,0 +1,14 @@
+import { memo } from "react";
+
+function ToggoleSounds({ allowSound, setAllowSound }) {
+  return (
+    <button
+      className="btn-sound"
+      onClick={() => setAllowSound((allow) => !allow)}
+    >
+      {allowSound ? "🔈" : "🔇"}
+    </button>
+  );
+}
+
+export default memo(ToggoleSounds);
